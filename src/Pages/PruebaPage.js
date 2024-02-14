ba99134158837ae08ce7d6aaea49dc1dec00bfa1
pascualmanuel <manuelnpascual@gmail.com>
2024-02-14 12:37:34 -0300

    Fixing log

--- src/Pages/PruebaPage.js
+++ src/Pages/PruebaPage.js
@@ -1,30 +1,30 @@
-import React, { useEffect, useRef } from "react";
-import { render } from "https://cdn.skypack.dev/react-dom"; // Import render from the correct location
-import Clients from "../Components/Clients"; // Update this with the correct file path
+// import React, { useEffect, useRef } from "react";
+// import { render } from "https://cdn.skypack.dev/react-dom"; // Import render from the correct location
+// import Clients from "../Components/Clients"; // Update this with the correct file path
 
-// import "../Styles/App.css";
-// import "../Styles/Prueba.css";
+// // import "../Styles/App.css";
+// // import "../Styles/Prueba.css";
 
-function PruebaPage() {
-  const appRef = useRef(null);
+// function PruebaPage() {
+//   const appRef = useRef(null);
 
-  useEffect(() => {
-    const ROOT_NODE = appRef.current;
+//   useEffect(() => {
+//     const ROOT_NODE = appRef.current;
 
-    // Render the App component inside the #app element
-    render(<Clients />, ROOT_NODE);
+//     // Render the App component inside the #app element
+//     render(<Clients />, ROOT_NODE);
 
-    // Clean up when the component is unmounted
-    return () => {
-      // Perform any cleanup if needed
-    };
-  }, []);
+//     // Clean up when the component is unmounted
+//     return () => {
+//       // Perform any cleanup if needed
+//     };
+//   }, []);
 
-  return (
-    <>
-      <div id="app" ref={appRef}></div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <div id="app" ref={appRef}></div>
+//     </>
+//   );
+// }
 
-export default PruebaPage;
+// export default PruebaPage;
